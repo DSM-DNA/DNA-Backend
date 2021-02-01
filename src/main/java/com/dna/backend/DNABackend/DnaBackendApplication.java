b7f3b8673dab788c925e053005d22909c23cea91
이승윤 <yyuunn17@naver.com>
2021-02-01 12:18:40 +0900

    [ADD] 기본 파일 추가

---
+++ src/main/java/com/dna/backend/DNABackend/DnaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dna.backend.DNABackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dna/backend/DNABackend/DnaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dna.backend.DNABackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
