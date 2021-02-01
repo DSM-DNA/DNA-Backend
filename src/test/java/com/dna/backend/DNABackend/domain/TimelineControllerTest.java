c2419b975b8088e838b87df3fedc8fb6f3fbaf1a
Hong Jung hyeon <jhhong0509@naver.com>
2021-02-01 18:00:43 +0900

    [FIX] commit bug

--- src/test/java/com/dna/backend/DNABackend/domain/TimelineControllerTest.java
+++ src/test/java/com/dna/backend/DNABackend/domain/TimelineControllerTest.java
@@ -1,7 +1,20 @@
 package com.dna.backend.DNABackend.domain;
 
+import com.dna.backend.DNABackend.DnaBackendApplication;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-//@ExtendWith()
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class TimelineControllerTest {
+
+    @AfterEach
+    public void cleanUp() {
+
+    }
+
 }
