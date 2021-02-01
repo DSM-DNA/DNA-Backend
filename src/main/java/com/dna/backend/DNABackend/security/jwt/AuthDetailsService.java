cecd1684142267be7ea3cf7802dfdae906f16637
이승윤 <yyuunn17@naver.com>
2021-02-01 23:34:44 +0900

    [ADD] Auth

---
+++ src/main/java/com/dna/backend/DNABackend/security/jwt/AuthDetailsService.java
@@ -0,0 +1,24 @@
+package com.dna.backend.DNABackend.security.jwt;
+
+import com.dna.backend.DNABackend.entity.user.UserRepository;
+import com.dna.backend.DNABackend.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AuthDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return userRepository.findByEmail(userEmail)
+                .map(AuthDetails::new)
+                .orElseThrow(UserNotFoundException::new);
+    }
+
+}
