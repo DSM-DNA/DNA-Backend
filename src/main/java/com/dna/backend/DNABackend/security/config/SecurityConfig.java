accd3faacb02a9e3c10d1dec8b12ad038e5a5cc0
이승윤 <yyuunn17@naver.com>
2021-02-02 12:30:09 +0900

    [UPDATE] config

--- src/main/java/com/dna/backend/DNABackend/security/config/SecurityConfig.java
+++ src/main/java/com/dna/backend/DNABackend/security/config/SecurityConfig.java
@@ -1,11 +1,18 @@
 package com.dna.backend.DNABackend.security.config;
 
+import com.dna.backend.DNABackend.security.jwt.JwtConfigure;
+import com.dna.backend.DNABackend.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,6 +20,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -24,7 +39,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
                 .authorizeRequests()
                     .antMatchers(HttpMethod.POST,"/signup").permitAll()
                     .antMatchers(HttpMethod.GET, "/email").permitAll()
-                    .antMatchers(HttpMethod.POST, "/auth").permitAll();
+                    .antMatchers(HttpMethod.POST, "/auth").permitAll()
+                    .anyRequest().authenticated().and()
+                .apply(new JwtConfigure(jwtTokenProvider));
     }
 
     @Override
@@ -32,6 +49,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("*");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
