40463820b88039a0daf84e5bcd498460352d6cbb
이승윤 <yyuunn17@naver.com>
2021-02-02 13:36:17 +0900

    [UPDATE] 이메일 검사 메소드 이동

--- src/main/java/com/dna/backend/DNABackend/service/user/UserServiceImpl.java
+++ src/main/java/com/dna/backend/DNABackend/service/user/UserServiceImpl.java
@@ -5,7 +5,9 @@ import com.dna.backend.DNABackend.entity.user.UserRepository;
 import com.dna.backend.DNABackend.exception.InvalidEmailAddressException;
 import com.dna.backend.DNABackend.exception.UserAlreadyExistsException;
 import com.dna.backend.DNABackend.payload.request.SignUpRequest;
+import lombok.CustomLog;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -13,11 +15,15 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService{
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public boolean confirmEmail(String email) {
+        if (!User.isValidAddress(email))
+            throw new InvalidEmailAddressException();
+
         if (userRepository.existsById(email)) {
-            return false;
+            throw new UserAlreadyExistsException();
         }else {
             return true;
         }
@@ -25,9 +31,6 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public void signUp(SignUpRequest signUpRequest) {
-        if (!signUpRequest.isValidAddress("dsm.hs.kr"))
-            throw new InvalidEmailAddressException();
-
         userRepository.findByEmail(signUpRequest.getEmail())
                 .ifPresent(user -> {
                     throw new UserAlreadyExistsException();
@@ -37,7 +40,7 @@ public class UserServiceImpl implements UserService{
                 User.builder()
                         .name(signUpRequest.getName())
                         .email(signUpRequest.getEmail())
-                        .password(signUpRequest.getPassword())
+                        .password(passwordEncoder.encode(signUpRequest.getPassword()))
                         .build()
         );
 
