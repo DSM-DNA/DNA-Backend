233e8b34b1ada0117db16d61dd9f46eae5938626
이승윤 <yyuunn17@naver.com>
2021-02-02 13:11:43 +0900

    [ADD] signIn

--- src/main/java/com/dna/backend/DNABackend/service/user/UserServiceImpl.java
+++ src/main/java/com/dna/backend/DNABackend/service/user/UserServiceImpl.java
@@ -1,6 +1,9 @@
 package com.dna.backend.DNABackend.service.user;
 
+import com.dna.backend.DNABackend.entity.user.User;
 import com.dna.backend.DNABackend.entity.user.UserRepository;
+import com.dna.backend.DNABackend.exception.InvalidEmailAddressException;
+import com.dna.backend.DNABackend.exception.UserAlreadyExistsException;
 import com.dna.backend.DNABackend.payload.request.SignUpRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -22,6 +25,21 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public void signUp(SignUpRequest signUpRequest) {
+        if (!signUpRequest.isValidAddress("dsm.hs.kr"))
+            throw new InvalidEmailAddressException();
+
+        userRepository.findByEmail(signUpRequest.getEmail())
+                .ifPresent(user -> {
+                    throw new UserAlreadyExistsException();
+                });
+
+        userRepository.save(
+                User.builder()
+                        .name(signUpRequest.getName())
+                        .email(signUpRequest.getEmail())
+                        .password(signUpRequest.getPassword())
+                        .build()
+        );
 
     }
 }
