d6e5248f0d6c720d3ef796928e6c94e674681d9f
이승윤 <yyuunn17@naver.com>
2021-06-30 10:13:23 +0900

    Update AccessTokenResponse.java

--- src/main/java/com/dna/backend/DNABackend/payload/response/AccessTokenResponse.java
+++ src/main/java/com/dna/backend/DNABackend/payload/response/AccessTokenResponse.java
@@ -6,7 +6,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class AccessTokenResponse {
 
-    @JsonProperty("access-token")
+    @JsonProperty("access_token")
     private final String accessToken;
 
 }
