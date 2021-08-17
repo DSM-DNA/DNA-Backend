ba5792f48a8a13c4a1d0879b608a9d6a993400e4
이승윤 <yyuunn17@naver.com>
2021-08-17 20:15:51 +0900

    [ADD] exception

---
+++ src/main/java/com/dna/backend/DNABackend/exception/VerifyCodeNotMatchException.java
@@ -0,0 +1,8 @@
+package com.dna.backend.DNABackend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Verify Code Is Not Match")
+public class VerifyCodeNotMatchException extends RuntimeException{
+}
