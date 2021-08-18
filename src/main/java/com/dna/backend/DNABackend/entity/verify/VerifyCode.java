07453f3af3c4ffef01b0705030351e58bf61763e
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-08-18 09:00:22 +0900

    [ADD] email

--- src/main/java/com/dna/backend/DNABackend/entity/verify/VerifyCode.java
+++ src/main/java/com/dna/backend/DNABackend/entity/verify/VerifyCode.java
@@ -1,6 +1,7 @@
 package com.dna.backend.DNABackend.entity.verify;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
@@ -9,6 +10,7 @@ import org.springframework.data.redis.core.RedisHash;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @RedisHash(timeToLive = 60 * 4)
 public class VerifyCode {
 
--- src/main/java/com/dna/backend/DNABackend/service/email/EmailServiceImpl.java
+++ src/main/java/com/dna/backend/DNABackend/service/email/EmailServiceImpl.java
@@ -1,16 +1,17 @@
 package com.dna.backend.DNABackend.service.email;
 
-import com.dna.backend.DNABackend.entity.user.User;
 import com.dna.backend.DNABackend.entity.user.UserRepository;
 import com.dna.backend.DNABackend.entity.verify.VerifyCode;
 import com.dna.backend.DNABackend.entity.verify.VerifyCodeRepository;
-import com.dna.backend.DNABackend.exception.InvalidEmailAddressException;
-import com.dna.backend.DNABackend.exception.UserAlreadyExistsException;
 import com.dna.backend.DNABackend.exception.VerifyCodeNotMatchException;
 import com.dna.backend.DNABackend.payload.request.EmailRequest;
 import com.dna.backend.DNABackend.payload.request.VerifyRequest;
 import com.dna.backend.DNABackend.sesconfig.ContentSender;
 import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
@@ -19,27 +20,54 @@ import java.util.Random;
 @Service
 public class EmailServiceImpl implements EmailService {
 
+    private static final Random RANDOM = new Random();
     private final UserRepository userRepository;
     private final VerifyCodeRepository verifyCodeRepository;
-
+    private final JavaMailSender javaMailSender;
     private final ContentSender contentSender;
 
-    private static final Random RANDOM = new Random();
-
+    @Async
     @Override
     public void sendVerifyCode(EmailRequest request) {
-        String email = request.getEmail();
-        if (!User.isValidAddress(email))
-            throw new InvalidEmailAddressException();
+        String authNum = generateVerifyCode();
 
-        if (userRepository.existsById(email)) {
-            throw new UserAlreadyExistsException();
+        try {
+            final MimeMessagePreparator preparator = mimeMessage -> {
+                final MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+                helper.setFrom("gramo@gmail.com");
+                helper.setTo(request.getEmail());
+                helper.setSubject("인증번호는 " + authNum + " 입니다");
+                helper.setText("Gramo");
+            };
+
+            javaMailSender.send(preparator);
+
+            verifyCodeRepository.findById(request.getEmail())
+                    .ifPresent(verifyCodeRepository::delete);
+
+            verifyCodeRepository.save(
+                    VerifyCode.builder()
+                            .email(request.getEmail())
+                            .verifyCode(authNum)
+                            .isVerified(false)
+                            .build()
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        verifyCodeRepository.findById(email)
-                .ifPresent(verifyCodeRepository::delete);
-
-        contentSender.sendMessage(email, generateVerifyCode());
+//        String email = request.getEmail();
+//        if (!User.isValidAddress(email))
+//            throw new InvalidEmailAddressException();
+//
+//        if (userRepository.existsById(email)) {
+//            throw new UserAlreadyExistsException();
+//        }
+//
+//        verifyCodeRepository.findById(email)
+//                .ifPresent(verifyCodeRepository::delete);
+//
+//        contentSender.sendMessage(email, generateVerifyCode());
     }
 
     @Override
